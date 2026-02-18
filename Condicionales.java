8b0827507d931f0e74709ad3947cbd86b32f753f
XavZol <alansolis707@gmail.com>
2026-02-17 19:37:15 -0600

    Condicionales con Switch.

--- Condicionales.java
+++ Condicionales.java
@@ -1,17 +1,27 @@
 
 import javax.swing.JOptionPane;
 
+/* Condicional con switch */
+
 public class Condicionales {
 
     public static void main(String[] args) {
-        int numero, dato = 5;
+        int dato;
 
-        numero = Integer.parseInt(JOptionPane.showInputDialog("Digite un numero: "));
+        dato = Integer.parseInt(JOptionPane.showInputDialog("Digite un numero entre 1 y 5: "));
 
-        if(numero == dato) {
-            JOptionPane.showMessageDialog(null, "El numero es 5");
-        } else {
-            JOptionPane.showMessageDialog(null, "El numero es diferente de 5");
+        switch(dato) {
+            case 1: JOptionPane.showMessageDialog(null, "Este es el numero 1");
+                break;
+            case 2: JOptionPane.showMessageDialog(null, "Este es el numero 2");
+                break;
+            case 3: JOptionPane.showMessageDialog(null, "Este es el numero 3");
+                break;
+            case 4: JOptionPane.showMessageDialog(null, "Este es el numero 4");
+                break;
+            case 5: JOptionPane.showMessageDialog(null, "Este es el numero 5");
+                break;
+            default: JOptionPane.showMessageDialog(null, "El numero no esta en el ragdo de 1 a 5");
         }
     }
 }
