f82ffd01cf683b2716295bd4cb70a9daf7568f97
XavZol <alansolis707@gmail.com>
2026-02-17 20:04:37 -0600

    Condicional de comparación de números.

--- Condicionales.java
+++ Condicionales.java
@@ -1,19 +1,24 @@
 
 import javax.swing.JOptionPane;
 
-/* Hacer un programa que lea un numero entero y muestre  si el numero entero es multiplo de 10 */
+
+
+/* Pedir dos numeros y decir cual es el mayor o si son iguales */
 
 public class Condicionales {
 
     public static void main(String[] args) {
-        int numero;
+        int numero1, numero2;
 
-        numero = Integer.parseInt(JOptionPane.showInputDialog("Digite un numero: "));
-
-        if(numero%10==0){
-            JOptionPane.showMessageDialog(null, "El numero "+numero+ " es multiplo de 10");
+        numero1 = Integer.parseInt(JOptionPane.showInputDialog("Digite un numero: "));
+        numero2 = Integer.parseInt(JOptionPane.showInputDialog("Digite otro numero: "));
+        
+        if(numero1 > numero2) {
+            JOptionPane.showMessageDialog(null, "El numero mayor es: "+numero1);
+        } else if(numero2 > numero1) {
+            JOptionPane.showMessageDialog(null, "El numero mayor es: "+numero2);
         } else {
-            JOptionPane.showMessageDialog(null, "El numero no es multiplo de 10");
+            JOptionPane.showMessageDialog(null, "Ambos numeros son iguales.");
         }
     }
 }
