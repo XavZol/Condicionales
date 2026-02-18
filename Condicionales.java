d793c5ce99965cbb9d81b6f4670b17f8a7a208c4
XavZol <alansolis707@gmail.com>
2026-02-17 19:57:55 -0600

    Programa que muestra si un numero es múltiplo de 10.

--- Condicionales.java
+++ Condicionales.java
@@ -1,21 +1,19 @@
 
 import javax.swing.JOptionPane;
 
-/* valor = (Condicion) ? valor1 : valor2 */
+/* Hacer un programa que lea un numero entero y muestre  si el numero entero es multiplo de 10 */
 
 public class Condicionales {
 
     public static void main(String[] args) {
         int numero;
-        String mensaje;
 
         numero = Integer.parseInt(JOptionPane.showInputDialog("Digite un numero: "));
 
-        mensaje = (numero%2==0) ? "Es par" : "Es impar";
-
-        JOptionPane.showMessageDialog(null, mensaje);
-
-        // Funciona de la misma manera
-        // JOptionPane.showMessageDialog(null, (numero%2==0) ? "Es par" : "Es impar");
+        if(numero%10==0){
+            JOptionPane.showMessageDialog(null, "El numero "+numero+ " es multiplo de 10");
+        } else {
+            JOptionPane.showMessageDialog(null, "El numero no es multiplo de 10");
+        }
     }
 }
